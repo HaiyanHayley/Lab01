bc93889bcd52a4a2d8f74ad0601b90bbc0b69313
z5135077 <z5135077@cse.unsw.edu.au>
2018-08-01 17:43:15 +1000

    test merge

---
+++ integer.py
@@ -0,0 +1,6 @@
+integers = [1, 2, 3, 4, 5]
+integers.append(6)
+total = 0
+for x in integers:
+    total=total+x
+print (total)
---
+++ strings.py
@@ -0,0 +1,5 @@
+strings = ['This', 'list', 'is', 'now', 'all', 'together']
+sentence = ''
+for x in strings:
+    sentence= sentence + x + ' '
+print(sentence[:-1])
